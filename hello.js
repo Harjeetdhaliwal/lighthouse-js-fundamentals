74a441d0a622b8a409813b06a0c38e4bef536ccd
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-03 14:55:29 -0400

    Return value from a function

--- hello.js
+++ hello.js
@@ -5,3 +5,17 @@ const sayHello = function(name){
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//------RETURN AND CONSOLE-------
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('john');
+console.log(greeting);
